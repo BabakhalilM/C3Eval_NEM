e18b677da30517db5ff555f6a32572dd673cf4fc
BabakhalilM <babakhalilmalyam2@gmail.com>
2024-07-08 18:18:12 +0530

    c3eval

--- Backend/connsection/db.js
+++ Backend/connsection/db.js
@@ -1,18 +1,9 @@
-import { Sequelize } from "sequelize";
-import { } from 'dotenv/config.js';
+// import { Sequelize } from "sequelize";
+// import {} from 'dotenv/config.js';
 
-const sequelize = new Sequelize(process.env.Sql_aiven_DB, process.env.Sql_aiven_user, process.env.Sql_aiven_password, {
-    host: process.env.Sql_aiven_Host,
-    dialect: 'mysql',
-});
+// const sequelize = new Sequelize(process.env.Sql_aiven_DB, process.env.Sql_aiven_user, process.env.Sql_aiven_password, {
+//     host: process.env.Sql_aiven_Host,
+//     dialect: 'mysql',
+// });
 
-export default sequelize;
-
-
-
-
-// sql_aiven_password=AVNS_82iqgZ92whrUtyO4JF2
-// Sql_aiven_user=avnadmin
-
-// Sql_aiven_Host= mysql-6ac7fe7-babakhalilmalyam2-fd25.h.aivencloud.com
-// Sql_aiven_port=23372
+// export default sequelize;
